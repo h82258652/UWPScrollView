7f345a459446b6e4b856b327a97d26249e6ece57
h82258652 <842053625@qq.com>
2024-07-12 12:08:23 +0800

    update

--- UWPScrollView/Microsoft/UI/Xaml/Controls/ScrollView.cs
+++ UWPScrollView/Microsoft/UI/Xaml/Controls/ScrollView.cs
@@ -1,4 +1,6 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using System.Numerics;
+using Windows.UI.Xaml.Controls;
 
 namespace Microsoft.UI.Xaml.Controls;
 
@@ -7,4 +9,26 @@ namespace Microsoft.UI.Xaml.Controls;
 /// </summary>
 public class ScrollView : Control
 {
+    /// <summary>
+    /// Asynchronously zooms by the specified delta amount with animations enabled and snap point respected.
+    /// </summary>
+    /// <param name="zoomFactorDelta">The amount by which to change the zoom factor.</param>
+    /// <param name="centerPoint">The center point of the zoom factor change.</param>
+    /// <returns>A correlation ID number used to associate this method call with corresponding events.</returns>
+    public int ZoomBy(float zoomFactorDelta, Vector2? centerPoint)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Asynchronously zooms by the specified delta amount with the specified animation and snap point modes.
+    /// </summary>
+    /// <param name="zoomFactorDelta">The amount by which to change the zoom factor.</param>
+    /// <param name="centerPoint">The center point of the zoom factor change.</param>
+    /// <param name="options">Options that specify whether or not animations are enabled and snap points are respected.</param>
+    /// <returns>A correlation ID number used to associate this method call with corresponding events.</returns>
+    public int ZoomBy(float zoomFactorDelta, Vector2? centerPoint, ScrollingZoomOptions options)
+    {
+        throw new NotImplementedException();
+    }
 }
---
+++ UWPScrollView/Microsoft/UI/Xaml/Controls/ScrollingZoomOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Microsoft.UI.Xaml.Controls;
+
+/// <summary>
+/// Represents options that can be set in zoom methods of the <see cref="ScrollView"/> control.
+/// </summary>
+public class ScrollingZoomOptions
+{
+}
