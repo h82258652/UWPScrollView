b67ddc25c87e5e3c2d3732aa4df469b0423e26d2
h82258652 <842053625@qq.com>
2024-07-12 12:00:22 +0800

    update

--- UWPScrollView/Microsoft/UI/Xaml/Controls/Primitives/ScrollPresenter.cs
+++ UWPScrollView/Microsoft/UI/Xaml/Controls/Primitives/ScrollPresenter.cs
@@ -1,20 +1,81 @@
 ﻿using System;
+using Windows.Foundation;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Markup;
 
 namespace Microsoft.UI.Xaml.Controls.Primitives;
 
+/// <summary>
+/// Represents a primitive container that provides scroll, pan, and zoom support for its content.
+/// </summary>
+[ContentProperty(Name = nameof(Content))]
 public class ScrollPresenter : FrameworkElement, IScrollAnchorProvider
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ScrollPresenter"/> class.
+    /// </summary>
+    public ScrollPresenter()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Occurs when the current interaction state of the control has changed.
+    /// </summary>
+    public event TypedEventHandler<ScrollPresenter, object> StateChanged;
+
+    /// <summary>
+    /// Occurs when manipulations such as scrolling and zooming have caused the view to change.
+    /// </summary>
+    public event TypedEventHandler<ScrollPresenter, object> ViewChanged;
+
+    /// <summary>
+    /// Gets or sets the content that can be scrolled, panned, or zoomed.
+    /// </summary>
+    public UIElement? Content
+    {
+        get
+        {
+            throw new NotImplementedException();
+        }
+        set
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// Gets the most recently chosen <see cref="UIElement"/> for scroll anchoring after a layout pass, if any.
+    /// </summary>
+    public UIElement CurrentAnchor => throw new NotImplementedException();
+
+    /// <summary>
+    /// Registers a <see cref="UIElement"/> as a potential scroll anchor.
+    /// </summary>
+    /// <param name="element">A <see cref="UIElement"/> within the subtree of the <see cref="ScrollPresenter"/>.</param>
     public void RegisterAnchorCandidate(UIElement element)
     {
         throw new NotImplementedException();
     }
 
-    public void UnregisterAnchorCandidate(UIElement element)
+    /// <summary>
+    /// Asynchronously scrolls by the specified delta amount with animations enabled and snap points respected.
+    /// </summary>
+    /// <param name="horizontalOffsetDelta">The offset to scroll by horizontally.</param>
+    /// <param name="verticalOffsetDelta">The offset to scroll by vertically.</param>
+    /// <returns>A correlation ID number used to associate this method call with corresponding events.</returns>
+    public int ScrollBy(double horizontalOffsetDelta, double verticalOffsetDelta)
     {
         throw new NotImplementedException();
     }
 
-    public UIElement CurrentAnchor => throw new NotImplementedException();
+    /// <summary>
+    /// Unregisters a <see cref="UIElement"/> as a potential scroll anchor.
+    /// </summary>
+    /// <param name="element">A <see cref="UIElement"/> within the subtree of the <see cref="ScrollView"/>.</param>
+    public void UnregisterAnchorCandidate(UIElement element)
+    {
+        throw new NotImplementedException();
+    }
 }
---
+++ UWPScrollView/Microsoft/UI/Xaml/Controls/ScrollingChainMode.cs
@@ -0,0 +1,22 @@
+﻿namespace Microsoft.UI.Xaml.Controls;
+
+/// <summary>
+/// Defines constants that specify how scroll- and zoom-chaining is handled by the <see cref="ScrollView"/> control.
+/// </summary>
+public enum ScrollingChainMode
+{
+    /// <summary>
+    /// The <see cref="ScrollView"/> chains to the closest scrollable/zoomable outer component, if any.
+    /// </summary>
+    Auto,
+
+    /// <summary>
+    /// The <see cref="ScrollView"/> acts as if chaining occurred even when no scrollable/zoomable outer component is present.
+    /// </summary>
+    Always,
+
+    /// <summary>
+    /// The <see cref="ScrollView"/> ignores any scrollable/zoomable outer component that is present and no chaining occurs.
+    /// </summary>
+    Never
+}
