d202f99f46ac29e3c0f9b632e21f5c72a929acad
h82258652 <842053625@qq.com>
2024-07-12 12:47:05 +0800

    update

---
+++ UWPScrollView/Microsoft/UI/Xaml/Controls/ScrollingSnapPointsMode.cs
@@ -0,0 +1,17 @@
+﻿namespace Microsoft.UI.Xaml.Controls;
+
+/// <summary>
+/// Defines constants that specify how the <see cref="ScrollView"/> handles snap points.
+/// </summary>
+public enum ScrollingSnapPointsMode
+{
+    /// <summary>
+    /// Snap points are respected.
+    /// </summary>
+    Default,
+
+    /// <summary>
+    /// Snap points are ignored.
+    /// </summary>
+    Ignore
+}
--- UWPScrollView/Microsoft/UI/Xaml/Controls/ScrollingZoomOptions.cs
+++ UWPScrollView/Microsoft/UI/Xaml/Controls/ScrollingZoomOptions.cs
@@ -1,8 +1,59 @@
-﻿namespace Microsoft.UI.Xaml.Controls;
+﻿using System;
+
+namespace Microsoft.UI.Xaml.Controls;
 
 /// <summary>
 /// Represents options that can be set in zoom methods of the <see cref="ScrollView"/> control.
 /// </summary>
 public class ScrollingZoomOptions
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ScrollingZoomOptions"/> class with the specified animation mode.
+    /// </summary>
+    /// <param name="animationMode">A value that indicates whether or not an animation is played for the zoom factor change.</param>
+    public ScrollingZoomOptions(ScrollingAnimationMode animationMode)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ScrollingZoomOptions"/> class with the specified animation and snap points modes.
+    /// </summary>
+    /// <param name="animationMode">A value that indicates whether or not an animation is played for the zoom factor change.</param>
+    /// <param name="snapPointsMode"></param>
+    /// <exception cref="NotImplementedException">A value that indicates whether snap points are ignored or respected during the zoom factor change.</exception>
+    public ScrollingZoomOptions(ScrollingAnimationMode animationMode, ScrollingSnapPointsMode snapPointsMode)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Gets or sets a value that indicates whether or not an animation is played for the zoom factor change.
+    /// </summary>
+    public ScrollingAnimationMode AnimationMode
+    {
+        get
+        {
+            throw new NotImplementedException();
+        }
+        set
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// Gets or sets a value that indicates whether snap points are ignored or respected during the zoom factor change.
+    /// </summary>
+    public ScrollingSnapPointsMode SnapPointsMode
+    {
+        get
+        {
+            throw new NotImplementedException();
+        }
+        set
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
