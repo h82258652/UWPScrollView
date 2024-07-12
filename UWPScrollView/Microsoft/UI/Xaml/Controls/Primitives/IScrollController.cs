ea57d261ee8cf795179b33badcc4e1b260abb861
h82258652 <842053625@qq.com>
2024-07-12 17:14:55 +0800

    update

--- UWPScrollView/Microsoft/UI/Xaml/Controls/Primitives/IScrollController.cs
+++ UWPScrollView/Microsoft/UI/Xaml/Controls/Primitives/IScrollController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Numerics;
+using Windows.Foundation;
 using Windows.UI.Composition;
 
 namespace Microsoft.UI.Xaml.Controls.Primitives;
@@ -8,6 +9,31 @@ namespace Microsoft.UI.Xaml.Controls.Primitives;
 /// </summary>
 public interface IScrollController
 {
+    /// <summary>
+    /// Occurs when a scroll velocity change is requested.
+    /// </summary>
+    event TypedEventHandler<IScrollController, ScrollControllerAddScrollVelocityRequestedEventArgs> AddScrollVelocityRequested;
+
+    /// <summary>
+    /// Occurs when the <see cref="CanScroll"/> property value has changed.
+    /// </summary>
+    event TypedEventHandler<IScrollController, object> CanScrollChanged;
+
+    /// <summary>
+    /// Occurs when the <see cref="IsScrollingWithMouse"/> property value changes.
+    /// </summary>
+    event TypedEventHandler<IScrollController, object> IsScrollingWithMouseChanged;
+
+    /// <summary>
+    /// Occurs when a scroll by an particular offset delta is requested.
+    /// </summary>
+    event TypedEventHandler<IScrollController, ScrollControllerScrollByRequestedEventArgs> ScrollByRequested;
+
+    /// <summary>
+    /// Occurs when a scroll to a particular offset is requested.
+    /// </summary>
+    event TypedEventHandler<IScrollController, ScrollControllerScrollToRequestedEventArgs> ScrollToRequested;
+
     /// <summary>
     /// Gets a value that indicates whether the user can scroll or pan with the scroll controller.
     /// </summary>
@@ -32,6 +58,18 @@ public interface IScrollController
     /// <param name="defaultAnimation">The animation that is applied to the scroll request.</param>
     /// <returns>The animation to use, if any, or <see langword="null"/>.</returns>
     CompositionAnimation? GetScrollAnimation(int correlationId, Vector2 startPosition, Vector2 endPosition, CompositionAnimation defaultAnimation);
+
+    /// <summary>
+    /// Indicates that a scrolling operation initiated through a <see cref="ScrollToRequested"/>, <see cref="ScrollByRequested"/>, or <see cref="AddScrollVelocityRequested"/> event has completed.
+    /// </summary>
+    /// <param name="correlationId">A correlation ID number used to associate a method call with corresponding events.</param>
+    void NotifyRequestedScrollCompleted(int correlationId);
+
+    /// <summary>
+    /// Sets a value that indicates whether or not the <see cref="ScrollPresenter"/> content is scrollable by means of user input.
+    /// </summary>
+    /// <param name="isScrollable"><see langword="true"/> if the <see cref="ScrollPresenter"/> content is scrollable by means of user input; otherwise, <see langword="false"/>.</param>
+    void SetIsScrollable(bool isScrollable);
 
     /// <summary>
     /// Provides dimension information about the scrolling control to the scroll controller.
---
+++ UWPScrollView/Microsoft/UI/Xaml/Controls/Primitives/ScrollControllerAddScrollVelocityRequestedEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.UI.Xaml.Controls.Primitives
+{
+    /// <summary>
+    /// Provides data for the <see cref="IScrollController.AddScrollVelocityRequested"/> event.
+    /// </summary>
+    public sealed class ScrollControllerAddScrollVelocityRequestedEventArgs
+    {
+    }
+}
