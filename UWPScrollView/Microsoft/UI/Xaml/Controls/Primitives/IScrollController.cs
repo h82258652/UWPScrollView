fa127311bd1690b344c53782d1d994fb7e67402b
h82258652 <842053625@qq.com>
2024-07-12 11:52:11 +0800

    update

--- UWPScrollView/Microsoft/UI/Xaml/Controls/Primitives/IScrollController.cs
+++ UWPScrollView/Microsoft/UI/Xaml/Controls/Primitives/IScrollController.cs
@@ -1,4 +1,7 @@
-﻿namespace Microsoft.UI.Xaml.Controls.Primitives;
+﻿using System.Numerics;
+using Windows.UI.Composition;
+
+namespace Microsoft.UI.Xaml.Controls.Primitives;
 
 /// <summary>
 /// Specifies a contract for scrollbar-like widgets that can set the scroll offsets of the content in a scrolling control.
@@ -17,6 +20,25 @@ public interface IScrollController
 
     /// <summary>
     /// Gets an instance of an <see cref="IScrollControllerPanningInfo"/> implementation that contains information related to content panning, or <see langword="null"/>.
-    /// </summary>    
+    /// </summary>
     IScrollControllerPanningInfo PanningInfo { get; }
+
+    /// <summary>
+    /// Provides the <see cref="IScrollController"/> with the option of customizing the animation used to perform its scroll request.
+    /// </summary>
+    /// <param name="correlationId">A correlation ID number used to associate a method call with corresponding events.</param>
+    /// <param name="startPosition">The start position of the content.</param>
+    /// <param name="endPosition">The end position of the content.</param>
+    /// <param name="defaultAnimation">The animation that is applied to the scroll request.</param>
+    /// <returns>The animation to use, if any, or <see langword="null"/>.</returns>
+    CompositionAnimation? GetScrollAnimation(int correlationId, Vector2 startPosition, Vector2 endPosition, CompositionAnimation defaultAnimation);
+
+    /// <summary>
+    /// Provides dimension information about the scrolling control to the scroll controller.
+    /// </summary>
+    /// <param name="minOffset">Specifies the minimum offset allowed for the relevant scrolling orientation.</param>
+    /// <param name="maxOffset">Specifies the maximum offset allowed for the relevant scrolling orientation.</param>
+    /// <param name="offset">Specifies the current offset value.</param>
+    /// <param name="viewportLength">Specifies the length of the scrolling control's viewport.</param>
+    void SetValues(double minOffset, double maxOffset, double offset, double viewportLength);
 }
