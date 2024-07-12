2e8882fc111eb2e7c9cd71a7bf1e1b22bfbc9acd
h82258652 <842053625@qq.com>
2024-07-12 14:16:41 +0800

    update

--- UWPScrollView/Microsoft/UI/Xaml/Controls/ScrollingZoomAnimationStartingEventArgs.cs
+++ UWPScrollView/Microsoft/UI/Xaml/Controls/ScrollingZoomAnimationStartingEventArgs.cs
@@ -1,9 +1,39 @@
-﻿namespace Microsoft.UI.Xaml.Controls;
+﻿using System;
+using Windows.UI.Composition;
+
+namespace Microsoft.UI.Xaml.Controls;
 
 /// <summary>
 /// Provides data for the <see cref="ScrollView.ZoomAnimationStarting"/> event.
 /// </summary>
 public sealed class ScrollingZoomAnimationStartingEventArgs
 {
-    private ScrollingZoomAnimationStartingEventArgs() { }
+    private ScrollingZoomAnimationStartingEventArgs()
+    { }
+
+    /// <summary>
+    /// Gets or sets the animation to run during the animated zoom factor change. The animation targets the content's scale.
+    /// </summary>
+    public CompositionAnimation Animation
+    {
+        get
+        {
+            throw new NotImplementedException();
+        }
+        set
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// Gets the content scale at the end of the animation.
+    /// </summary>
+    public float EndZoomFactor
+    {
+        get
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
